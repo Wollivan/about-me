bbaa1154277ef83529431f2ee8867c6bcf3b9f18
Wollivan <tim.smith2814@gmail.com>
2022-08-18 10:33:13 +0100

    Change folder structure

--- js/username.js
+++ js/username.js
@@ -1,6 +1,5 @@
 function getUsername() {
   let username = prompt("What is your name");
-  alert("Hi " + username);
 
   return document.write("Hey there " + username + ", so glad you're here!");
 }
